d9058bf2023959cf45113d43b2e90ef0b0ed7c53
Sergio Yamazaki <sergio.hideki@lactec.com.br>
2026-01-13 11:40:56 -0300

    Exercício 18: Fechar a Intranet

---
+++ backend/src/lactec/intranet/upgrades/workflow.py
@@ -0,0 +1,14 @@
+from lactec.intranet import logger
+from plone import api
+from Products.CMFPlone.WorkflowTool import WorkflowTool
+from Products.GenericSetup.tool import SetupTool
+
+
+def atualiza_permissoes(portal_setup: SetupTool):
+    """Atualiza todas as permissões em vista do novo workflow."""
+    # Utilizamos a tool que gerencia todos os workflows
+    wf_tool: WorkflowTool = api.portal.get_tool("portal_workflow")
+    # Atualiza permissões
+    wf_tool.updateRoleMappings()
+    # Loga que modificação foi realizada
+    logger.info("Permissões de workflow atualizadas")
